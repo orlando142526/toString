1811c4d0feb3dad13aa9c495722e851013281110
DANIELCASTELAO\oyagualendara <oyagualendara@daniecastelao.org>
2019-11-26 10:34:18 +0100

    metodo tostring

---
+++ triangulo/Metodos.java
@@ -0,0 +1,44 @@
+
+package triangulo;
+
+
+public class Metodos {
+    
+    private float base;
+    private float altura;
+
+    public Metodos() {
+    }
+
+    public Metodos(float base, float altura) {
+        this.base = base;
+        this.altura = altura;
+    }
+
+    public float getBase() {
+        return base;
+    }
+
+    public void setBase(float base) {
+        this.base = base;
+    }
+
+    public float getAltura() {
+        return altura;
+    }
+
+    public void setAltura(float altura) {
+        this.altura = altura;
+    }
+
+    @Override
+    public String toString() {
+        return "Metodos{" + "base=" + base + ", altura=" + altura + '}';
+    }
+    public void amosar(){
+        System.out.println(toString());
+    }
+    
+    
+    
+}
---
+++ triangulo/Triangulo.java
@@ -0,0 +1,15 @@
+
+package triangulo;
+
+
+public class Triangulo {
+
+  
+    public static void main(String[] args) {
+     
+        Metodos obx = new Metodos(2.3f,5);
+        obx.amosar();
+        
+    }
+    
+}
